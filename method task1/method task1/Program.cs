7b4c772ea08173fc70707a7d1f1279d704a0e745
Vikramjeet singh Singh <30014464@student.toiohomai.ac.nz>
2019-05-08 11:32:44 +1200

    msg

---
+++ method task1/method task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Title*/
+            Console.WriteLine("********Task1********\n");
+            /*obtain the height from the user*/
+            Console.Write("Please enter your height in meters: ");
+            double h = double.Parse(Console.ReadLine());
+            /*obtain the weight from the user*/
+            Console.Write("Please enter your weight in kg's: ");
+            double w = double.Parse(Console.ReadLine());
+
+            /*Display the result*/
+            Console.WriteLine("Your BMI is: " + BMI(w, h));
+        }
+        static double BMI(double w, double h)                                                                                                       
+        {
+        return Math.Round(w/(h*h),1);
+        }
+        /******** Task 2 Methods *********/
+        static void Task2(double bmi)
+        {
+            if (bmi >= 30)
+                Console.WriteLine("Your fat bro...");
+            else if (bmi >= 25 && bmi < 30)
+                Console.WriteLine("You need to loose some weight...");
+            else if (bmi >= 18.5 && bmi < 25)
+                Console.WriteLine("oooosh...");
+            else
+                Console.WriteLine("You to skinny...");
+        }
+    }
+}
---
+++ methods/methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********Question2*********");
+            Console.Write("Please enter a distance in miles: ");
+            double input = double.Parse(Console.ReadLine());
+
+
+
+
+
+            {
+                Console.WriteLine("******QUS3******");
+                    Console.Write("Please enter the radius of the circle: ");
+                double input = double.Parse(Console.ReadLine());
+                Circumference(double.Parse(Console.ReadLine()));
+                Console.WriteLine("The circumference of a circle with a radius of " + input + " is " + Math.Round(circum
+                Console.WriteLine("The area of a circle with a radius of " + input + " is " + Math.Round(Area(input)));
+            }
+             double Circumference(double rad)
+            {
+                Console.WriteLine("**********QUS4*******");
+                Console.Write("Please enter a number from 1 - 7: ");
+                Console.WriteLine("\nThat number corrresponds  to the day " + Day(int.Parse(Console.ReadLine())));
+            } 
+                static string Day(int num)
+                string[] day = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "saturday", "Sunday" };
+            return day[num - 1];
+
+              }
+    }
+}
